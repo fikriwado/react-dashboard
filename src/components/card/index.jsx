66144db9fc47edcbf36aa267a59819c2d5e95b39
fikriwado <fixwad.online@gmail.com>
2024-02-23 17:25:11 +0700

    blueprint widget card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,5 +1,23 @@
-const Index = () => {
-  return <div>Card</div>
+import './scss/_card.scss'
+
+const Index = ({ children }) => <div className='react-card'>{children}</div>
+
+const Title = ({ children, size = 'medium', align }) => {
+  const headingSize = `react-card__title--${size}`
+  return (
+    <div className='react-card__title'>
+      <h1 className={`${headingSize} ${align ? `r-text-center` : ''}`}>
+        {children}
+      </h1>
+    </div>
+  )
 }
+
+const Body = ({ children }) => (
+  <div className='react-card__body'>{children}</div>
+)
+
+Index.Title = Title
+Index.Body = Body
 
 export default Index
--- src/components/widgetbar/index.jsx
+++ src/components/widgetbar/index.jsx
@@ -1,9 +1,36 @@
 import './scss/_widgetbar.scss'
+import Card from '../card'
 
 const Index = () => {
   return (
     <div className='react-widget'>
-      <div className='react-widget__header'>Widget</div>
+      <Card>
+        <Card.Title size='large'>Expenses and income</Card.Title>
+        <Card.Body>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+          enim.
+        </Card.Body>
+      </Card>
+
+      <Card>
+        <Card.Title size='large'>Latest spending</Card.Title>
+        <Card.Body>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          laboriosam, odio nihil ex facilis sed similique dignissimos rem sequi
+          tempore!
+        </Card.Body>
+      </Card>
+
+      <Card>
+        <Card.Title size='small' align='center'>
+          Go to premium
+        </Card.Title>
+        <Card.Body>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          laboriosam, odio nihil ex facilis sed similique dignissimos rem sequi
+          tempore!
+        </Card.Body>
+      </Card>
     </div>
   )
 }
